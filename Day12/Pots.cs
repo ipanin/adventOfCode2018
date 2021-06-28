1c349c20410090da490bfebb840d0594e09d0897
Ilya Panin <ilya.panin@kaspersky.com>
2021-06-28 14:58:32 +0300

    Day 12 refactoring

--- Day12/Pots.cs
+++ Day12/Pots.cs
@@ -7,15 +7,56 @@ using NUnit.Framework;
 
 namespace AdventOfCode.Y2018.Day12
 {
-	class Day12Solver
+	class Pots
 	{
-		public static (BitArray initial, bool[] rules) ParseInput(string[] rawInput) {
+		private BitArray _initial;
+		private bool[] _rules;
+		private int _maxLen;
+		private BitArray _generation;
+		private long _generationNumber;
+
+		public Pots(string[] rawInput) {
 			var input = rawInput.Select(s => 
 				s.Replace('.', '0').Replace('#', '1'));
 			
-			var initial = GetInitialState(input.First());
-			var rules = GetRules(input.Skip(2));
-			return (initial, rules);
+			_initial = GetInitialState(input.First());
+			_maxLen = _initial.Length;
+			_rules = GetRules(input.Skip(2));
+			
+			InitFirstGeneration();
+		}
+
+		public long Solve(long numGenerations) {
+			const long SustainableGeneration = 125; // visually found that after g=125 picture only moves right
+
+			PrintGeneration();
+
+			for (long gen = 1; gen <= numGenerations; gen++) {
+				NextGeneration();
+				PrintGeneration();
+				if (gen == SustainableGeneration)
+					break;
+			}
+
+			long sum = 0;
+			var positiveCount = 0;
+			for (int i = 0; i < _generation.Length; i++)
+				if (_generation[i]) {
+					sum += i - _maxLen;
+					positiveCount++;
+				}
+
+			if (numGenerations > 20)
+				sum = sum + (numGenerations - SustainableGeneration) * positiveCount;
+
+			return sum;
+		}
+
+		private void InitFirstGeneration() {
+			_generationNumber = 0;
+			_generation = new BitArray(_maxLen * 6, false);
+			for (int i = 0; i < _maxLen; i++)
+				_generation[i + _maxLen] = _initial[i];
 		}
 
 		// initial state: #..######..#....#####..
@@ -37,55 +78,25 @@ namespace AdventOfCode.Y2018.Day12
 
 			return result;
 		}
-
-		public static long Solve(BitArray initial, bool[] rules, long numGenerations) {
-			const long SustainableGeneration = 125; // visually found that after g=125 picture only moves right
-
-			int maxLen = initial.Length;
-			var generation = new BitArray(maxLen * 6, false);
-			for (int i = 0; i < maxLen; i++)
-				generation[i + maxLen] = initial[i];
-			
-			PrintGeneration(0, generation, maxLen);
-
-			for (long g = 1; g <= numGenerations; g++) {
-				generation = NextGeneration(rules, generation);
-				PrintGeneration(g, generation, maxLen);
-				if (g == SustainableGeneration)
-					break;
-			}
-
-			long sum = 0;
-			var positiveCount = 0;
-			for (int i = 0; i < generation.Length; i++)
-				if (generation[i]) {
-					sum += i - maxLen;
-					positiveCount++;
-				}
-
-			if (numGenerations > 20)
-				sum = sum + (numGenerations - SustainableGeneration) * positiveCount;
-
-			return sum;
-		}
-
-		static void PrintGeneration(long num, BitArray generation, int maxLen) {
-			Console.Write($"{num}: ");
-			for (int i = maxLen; i < 4 * maxLen; i++)
-				Console.Write(generation[i] ? '|' : '.');
+		
+		private void PrintGeneration() {
+			Console.Write($"{_generationNumber}: ");
+			for (int i = _maxLen; i < 4 * _maxLen; i++)
+				Console.Write(_generation[i] ? '|' : '.');
 			Console.WriteLine();
 		}
 
-		static BitArray NextGeneration(bool[] rules, BitArray gen) {
-			var result = (BitArray) gen.Clone();
-			for (int i = 2; i < gen.Length - 2; i++) {
+		private void NextGeneration() {
+			var result = (BitArray) _generation.Clone();
+			for (int i = 2; i < _generation.Length - 2; i++) {
 				var pattern = new BitVector32(0);
 				for (int j = 0; j < 5; j++)
-					pattern[1 << j] = gen[4 - j + i - 2];
-				result[i] = rules[pattern.Data];
+					pattern[1 << j] = _generation[4 - j + i - 2];
+				result[i] = _rules[pattern.Data];
 			}
 
-			return result;
+			_generation = result;
+			_generationNumber++;
 		}
 	}
 	
@@ -93,10 +104,10 @@ namespace AdventOfCode.Y2018.Day12
 		[TestCase("Day12/sample.txt", 20, 325)]
 		[TestCase("Day12/input.txt", 20, 2736)] // part1
 		[TestCase("Day12/input.txt", 50_000_000_000, 3150000000905)] // part2
-		public void Part1Test(string fileName, long numGenerations, long expectedSum) {
+		public void Test(string fileName, long numGenerations, long expectedSum) {
 			var rawInput = Utils.GetTestStrings(fileName);
-			var input = Day12Solver.ParseInput(rawInput);
-			var answer = Day12Solver.Solve(input.initial, input.rules, numGenerations);
+			var pots = new Pots(rawInput);
+			var answer = pots.Solve(numGenerations);
 			Assert.AreEqual(expectedSum, answer, "Wrong answer");
 		}
 	}
